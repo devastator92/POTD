1915551531f3b94cb8db0908810636a1c743f7b6
Ariel Conejeros <ariel.conejeros.munoz@ciisa.cl>
2021-11-21 14:17:48 -0300

    Update PotdHellkro.py

--- PotdHellkro.py
+++ PotdHellkro.py
@@ -9,14 +9,16 @@ layout="centered",
 initial_sidebar_state="expanded",
 )
 
-url = 'https://api.nasa.gov/planetary/apod?api_key=' + st.secrets["MY_NASA_APIKEY"]
+url = 'https://api.nasa.gov/planetary/apod?api_key=' + st.secrets["NASA_APIKEY"]
 
 response = requests.get(url)
 data = response.json()
-st.title('Picture of the Day', anchor='center')
+st.markdown("<h1 style='text-align: center; color: black;'>NASA Picture of the Day</h1>", unsafe_allow_html=True)
 
 image = Image.open(requests.get(data['url'], stream=True).raw)
 st.image(image, caption=data['title'], use_column_width=True)
-st.write(data['explanation'])
+explanation = data['explanation']
+st.markdown("<p style='text-align: justify; color: black;'>" + explanation + "</p>", unsafe_allow_html=True)
 st.write(data['copyright'])
 st.write(data['date'])
+
