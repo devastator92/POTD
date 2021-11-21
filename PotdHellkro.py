5e7fc669c4d347854b889fad2f195d5201824db4
Ariel Conejeros <ariel.conejeros.munoz@ciisa.cl>
2021-11-20 23:48:44 -0300

    .

--- PotdHellkro.py
+++ PotdHellkro.py
@@ -18,8 +18,6 @@ url = 'https://api.nasa.gov/planetary/apod?api_key=WJsoSmfRyWRqydUZjIqIcuGeOasuL
 response = requests.get(url)
 data = response.json()
 st.title('Astronomy Picture of the Day', anchor='center')
-st.header('NASA Astronomy Picture of the Day')
-st.subheader('Socio, usando Streamlit y python jojojojo')
 
 image = Image.open(requests.get(data['url'], stream=True).raw)
 st.image(image, caption=data['title'], use_column_width=True)
