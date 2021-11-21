737fa481d32b420865e233e742d4512af6a32570
Ariel Conejeros <ariel.conejeros.munoz@ciisa.cl>
2021-11-21 14:06:25 -0300

    Update PotdHellkro.py

--- PotdHellkro.py
+++ PotdHellkro.py
@@ -1,17 +1,13 @@
-#importar streamlit, requests y PIL
 import streamlit as st
 import requests
 from PIL import Image
 
 st.set_page_config(
-    page_title="Picture of the Day",
-    page_icon="rocket",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"})
+page_title="Picture of the Day",
+page_icon="rocket",
+layout="centered",
+initial_sidebar_state="expanded",
+)
 
 url = 'https://api.nasa.gov/planetary/apod?api_key=' + st.secrets["MY_NASA_APIKEY"]
 
@@ -24,4 +20,3 @@ st.image(image, caption=data['title'], use_column_width=True)
 st.write(data['explanation'])
 st.write(data['copyright'])
 st.write(data['date'])
-
